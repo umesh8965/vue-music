e38e43226045b6bd941efc10bbb6cb271dc9fd4b
umesh8965 <umesh_8965@yahoo.co.in>
2021-01-30 15:27:11 +0530

    VM-2 : Create Playlist

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/auth/Login.vue'
 import Signup from '../views/auth/Signup.vue'
+import CreatePlaylist from '../views/playlist/CreatePlaylist.vue'
+
 
 const routes = [
   {
@@ -18,6 +20,11 @@ const routes = [
     path: '/signup',
     name: 'Signup',
     component: Signup
+  },
+  {
+    path: '/playlist/create',
+    name: 'CreatePlaylist',
+    component: CreatePlaylist
   }
 ]
 
